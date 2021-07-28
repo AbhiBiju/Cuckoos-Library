4977ff10347fe61b63a0c6312625e0dd16853a75
AbhiBiju <abhinavbiju29@gmail.com>
2021-07-28 14:25:00 -0400

    finalize script and add delete function

---
+++ assets/js/savedBooks.js
@@ -0,0 +1,53 @@
+var booksArray = JSON.parse(localStorage.getItem("savedBooks"));
+console.log(booksArray);
+
+var booksBox = $("#booksBox");
+booksArray.forEach((book) => {
+  var newBook = $(`${book}`);
+  var newCard = $('<div class="card">');
+
+  newCard.append(newBook);
+
+  booksBox.append(newCard);
+});
+
+$(document).ready(() => {
+  var grabFooter = $(".card-footer");
+  var deletThis = $('<button style="padding:0.7rem;margin:1rem;" class="delete is-small"></button>');
+  // grabFooter.attr("style", "display:flex; align-items:center;");
+  grabFooter.append(deletThis);
+
+  $(document).on("click", ".delete", (event) => {
+    var footer = event.target.parentElement;
+    var card = footer.parentElement;
+
+    for (book of booksArray) {
+      // book = "";
+      console.log(
+        `${book.slice(
+          0,
+          -15
+        )}<button style="padding:0.7rem;margin:1rem;" class="delete is-small"></button></footer></div>`
+      );
+
+      if (
+        card.outerHTML ===
+        `${book.slice(
+          0,
+          -15
+        )}<button style="padding:0.7rem;margin:1rem;" class="delete is-small"></button></footer></div>`
+      ) {
+        booksArray.splice(book, 1);
+      }
+      console.log(booksArray);
+    }
+
+    localStorage.setItem("savedBooks", JSON.stringify(booksArray));
+    card.remove();
+  });
+  $(document).on("click", "#clear-btn", () => {
+    booksArray = [];
+    localStorage.setItem("savedBooks", JSON.stringify(booksArray));
+    booksBox.html("");
+  });
+});
--- assets/js/script.js
+++ assets/js/script.js
@@ -17,15 +17,15 @@ bookSearchBtn.click(async () => {
 
   var apiURL = "https://www.googleapis.com/books/v1/volumes?q=";
   var apiKey = "&key=AIzaSyCcXM9xp7ZReVIVy-jlgcN7S1C2U6gmlH4";
-  
+
   if (titleValue != "") {
     apiURL += titleValue;
   }
   if (authorValue != "") {
-    apiURL+=`+inauthor:${authorValue}`;
+    apiURL += `+inauthor:${authorValue}`;
   }
   if (genreValue != "") {
-    apiURL+=`+subject:${genreValue}`;
+    apiURL += `+subject:${genreValue}`;
   }
   apiURL.concat(apiKey);
 
@@ -34,39 +34,223 @@ bookSearchBtn.click(async () => {
   const response = await fetch(`${apiURL}`);
   const data = await response.json();
 
-  var topFive = data.items.splice(0, 5);
+  var topTen = data.items;
 
-  topFive.forEach((item) => {
+  topTen.forEach((item) => {
     if (item.volumeInfo.imageLinks != undefined) {
-      var newImg = $(`<img src="${item.volumeInfo.imageLinks.thumbnail}">`);
-      newBooks.append(newImg);
+      var newCard = $('<div class="card">');
+
+      var cardImage = $(`<div class="card-image">`);
+      var cardFigure = $(`<figure class="image is-128by128">`);
+      var newImg = $(`<img class="imgs" src="${item.volumeInfo.imageLinks.thumbnail}">`);
+      cardFigure.append(newImg);
+      cardImage.append(cardFigure);
+      newCard.append(cardImage);
+
+      var newContent = $(`<div class="media-content">`);
+
+      var newTitle = $(`<p class="title is-4">`);
+      newTitle.text(`Title: ${item.volumeInfo.title}`);
+      newContent.append(newTitle);
+
+      var newSubTitle = $(`<p class="subtitle is-6">`);
+      newSubTitle.text(`Author: ${item.volumeInfo.authors[0]}`);
+      newContent.append(newSubTitle);
+
+      newCard.append(newContent);
+
+      var newFooter = $('<footer class="card-footer">');
+      var saveBtn = $(` <a class="card-footer-item has-text-weight-bold saveBtn">Save</a>`);
+      newFooter.append(saveBtn);
+      newCard.append(newFooter);
+
+      newBooks.append(newCard);
     }
   });
 });
 
-var gifTester = ["Boats", "Running", "Train", "Cave", "Robot", "Gears", "Apple"];
+var genreGifs = ["Theater", "Reading", "Colors", "Daydreaming", "Vacation"];
 
-var userAnswers = {
-  horror: 0,
-  action: 0,
-  scifi: 0,
-};
+const questions = [
+  {
+    text: "What kind of movies do you like the most?",
+    answers: [
+      "A. Something with tons of gore, and that'll make me have nightmares.",
+      "Something that I'll need to think about or solve. May or may not have plot twists.",
+      "Something heartwarming, where love is the focus.",
+      "Something set in the future or another planet with futuristic technology.",
+      "Something about a real person or an event that may or may not be a documentary.",
+      "Something with a lot of action! I dislike the boring crap...",
+    ],
+    genre: ["Horror", "Mystery", "Romance", "SciFi", "Nonfiction", "Action"],
+  },
+  {
+    text: "What's your favorite part about reading a book?",
+    answers: [
+      "I love when books make my heart beat faster.",
+      "I love when a book keeps me guessing.",
+      "I love when a book tugs on my heartstrings.",
+      "I love when a book makes me envision the future.",
+      "I love when a book informs me on real events & people.",
+      "I love when a book gives me a good thrill/adventurous feeling.",
+    ],
+    genre: ["Horror", "Mystery", "Romance", "SciFi", "Nonfiction", "Action"],
+  },
+  {
+    text: "Which of these colors do you prefer?",
+    answers: ["Black", "Blue", "Purple", "Green", "Brown", "Red"],
+    genre: ["Horror", "Mystery", "Romance", "SciFi", "Nonfiction", "Action"],
+  },
+  {
+    text: "What do you usually daydream about?",
+    answers: [
+      "My daydreams are nightmares",
+      "Solving crimes",
+      "Love & romantic scenes",
+      "The future/outer space",
+      "Meeting famous historical figures",
+      "Going on adventures and kicking butt",
+    ],
+    genre: ["Horror", "Mystery", "Romance", "SciFi", "Nonfiction", "Action"],
+  },
+  {
+    text: "Which of the following sounds like a great getaway?",
+    answers: [
+      "A secluded cabin in the woods.",
+      "A lavish hotel in a city shrouded in mystery.",
+      "A resort, preferably with a lover",
+      "Another planet.",
+      "Ruins in another country",
+      "Any fast-paced inner city like New York",
+    ],
+    genre: ["Horror", "Mystery", "Romance", "SciFi", "Nonfiction", "Action"],
+  },
+];
 
-var nextBtn = $("#nextBtn");
+var questionNumber = 0;
+var startBtn = $("#startBtn");
+
+var scores = { Horror: 0, Mystery: 0, Romance: 0, SciFi: 0, Nonfiction: 0, Action: 0 };
+
+//Create a div in index.html with class questionsContainer, thats where the question text will reside
+
+function nextQuestion() {
+  var questionsContainer = $("#question");
+  if (!questions[questionNumber]) {
+    questionsContainer.html("");
+    return endQuiz();
+  }
+
+  questionsContainer.text("");
+
+  questionsContainer.text(questions[questionNumber].text);
+
+  //Loop that displays answer buttons
+  var topics = $("#topics");
+
+  for (let i = 0; i < questions.length; i++) {
+    var answerButton = $(
+      `<button class="${questions[questionNumber].genre[i]} button is-rounded is-size-6 is-medium">`
+    );
+
+    answerButton.text(`${questions[questionNumber].answers[i]}`);
+
+    topics.append(answerButton);
+
+    //Move to next question upon answer clicknNumber++
+    answerButton.on("click", function (event) {
+      var genre = event.target.classList[0];
+      scores[genre]++;
+      topics.html("");
+
+      questionNumber++;
+      nextQuestion();
+      nextGif();
+    });
+  }
+}
+
+function endQuiz() {
+  var highScore = -1;
+  var genreMatch = "";
+
+  for (key in scores) {
+    if (scores[key] > highScore) {
+      highScore = scores[key];
+      genreMatch = key;
+    }
+  }
+
+  var questionsBox = $("#question");
+  questionsBox.html(
+    `Your Favorite Genre is: <button id="finalBtn" class="button is-rounded is-medium">${genreMatch}</button>`
+  );
+
+  async function getBooks() {
+    var apiKey = "&key=AIzaSyCcXM9xp7ZReVIVy-jlgcN7S1C2U6gmlH4";
+    var genreBooks = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${genreMatch}${apiKey}`);
+    var objectify = await genreBooks.json();
+
+    var newDiv = $("#gif-placeholder");
+    newDiv.html(" ");
+    newDiv.attr(
+      "style",
+      "display:flex; flex-flow:column wrap; gap:2rem; overflow-x:scroll; overflow-y:scroll;"
+    );
+
+    var topTen = objectify.items;
+
+    topTen.forEach((item) => {
+      if (item.volumeInfo.imageLinks != undefined) {
+        var newCard = $('<div class="card" style="width:10rem">');
+
+        var cardImage = $(`<div class="card-image">`);
+        var cardFigure = $(`<figure class="image is-128by128">`);
+        var newImg = $(`<img class="imgs" src="${item.volumeInfo.imageLinks.thumbnail}">`);
+        cardFigure.append(newImg);
+        cardImage.append(cardFigure);
+        newCard.append(cardImage);
+
+        var newContent = $(`<div class="media-content">`);
+
+        var newTitle = $(`<p class="title is-4">`);
+        newTitle.text(`Title: ${item.volumeInfo.title}`);
+        newContent.append(newTitle);
+        console.log(newTitle);
+
+        var newSubTitle = $(`<p class="subtitle is-6">`);
+        newSubTitle.text(`Author: ${item.volumeInfo.authors[0]}`);
+        newContent.append(newSubTitle);
+        console.log(newSubTitle);
+
+        newCard.append(newContent);
+
+        var newFooter = $('<footer class="card-footer">');
+        var saveBtn = $(` <a class="card-footer-item has-text-weight-bold saveBtn">Save</a>`);
+        newFooter.append(saveBtn);
+        newCard.append(newFooter);
+        console.log(newCard);
+
+        newDiv.append(newCard);
+        console.log(newDiv);
+      }
+    });
+  }
+
+  $(document).on("click", "#finalBtn", () => {
+    getBooks();
+  });
+}
+
 var gifBox = $("#topicGif");
-var topic = $("#topic");
 
 var gifIndex = 0;
 async function nextGif() {
-  console.log(gifTester[gifIndex]);
-  topic.text(gifTester[gifIndex]);
-
   const getGif = await fetch(
-    `https://api.giphy.com/v1/gifs/search?q=${gifTester[gifIndex]}&api_key=7q56uzHnKHnzF8yVCo24CIsbKGGE2klH`
+    `https://api.giphy.com/v1/gifs/search?q=${genreGifs[gifIndex]}&api_key=7q56uzHnKHnzF8yVCo24CIsbKGGE2klH`
   );
 
   const gifData = await getGif.json();
-  console.log(gifData);
 
   var randomGif = Math.round(Math.random() * gifData.data.length);
 
@@ -74,11 +258,25 @@ async function nextGif() {
   gifBox.attr("src", gifURL);
 
   gifIndex++;
-  if (gifIndex === gifTester.length) {
+  if (gifIndex === genreGifs.length) {
     gifIndex = 0;
   }
-  console.log(gifIndex);
 }
-nextBtn.click(nextGif);
 
-var questionsContainer = $("#question");
+startBtn.click((event) => {
+  nextQuestion();
+  nextGif();
+  event.target.parentElement.remove();
+});
+
+var srcArray = [];
+
+$(document).on("click", ".saveBtn", (event) => {
+  var saveButton = event.target;
+  var footer = saveButton.parentElement;
+  var card = footer.parentElement;
+  srcArray.push(card.outerHTML);
+  console.log(srcArray);
+  // images.forEach((image) => {});
+  localStorage.setItem("savedBooks", JSON.stringify(srcArray));
+});
